f04a21f7dd393c916c55013da95788d5815feb47
Kit_Kat <matafeii38@gmail.com>
2026-03-09 14:01:35 +0200

    redacted

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: '.',
+    root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./*.html'),
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -39,7 +39,7 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      FullReload(['./**/*.html']),
+      FullReload(['./src/**/**.html']),
       SortCss({
         sort: 'mobile-first',
       }),
